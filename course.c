6bd1336f2d97b296380a2b5e64d2547c26bfa551
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2019-12-05 01:26:47 +0100

    Clean commit

--- course.c
+++ course.c
@@ -1,32 +1,17 @@
 #include <stdio.h>
 #include "course.h"
 
-struct course {
-	int course_num;
-	char* course_name;
-	int semester_num;
-	course_t next;
-};
-course_t create_course(int course_num, char* course_name, int semester_num) {
 
-}
 
-void destroy_course(course_t self) {
-
-}
-
-int get_course_numb(course_t self) {
-
-}
-int get_semester_num(course_t self) {
-
-}
-char* get_course_name(course_t self) {
-
-}
-course_t get_nextCourse_address(course_t self) {
-
-}
-void set_nextCourse_address(course_t self, course_t next) {
+pcourse_t createCourse(int course_no, char* course_name, int semester_no){}
+void destroy_course(pcourse_t course){}
+void set_cours_num(pcourse_t course, int course_num){}
+void set_course_name(pcourse_t course, char* course_name){}
+void set_semester_num(pcourse_t course, int semester_num){}
+int get_course_num(pcourse_t course){}
+char get_course_name(pcourse_t course){}
+int get_semester_num(pcourse_t course){}
+void print_course_info(pcourse_t course) {
+	printf("T \t %s\t %d\n", course->course_name, course->course_num);
 
 }
--- course.h
+++ course.h
@@ -2,14 +2,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct course* course_t;
+typedef struct course_t* pcourse_t;
 
-course_t create_course(int course_num, char* course_name, int semester_num);
+typedef struct course_t {
+	//C course_number course_name semester_number
+	int course_num;
+	char* course_name;
+	int semester_num;
+	//	pcourse_t next;
+}course_t;
 
-void destroy_course(course_t self);
-
-int get_course_numb(course_t self);
-int get_semester_num(course_t self);
-char* ge_course_name(course_t self);
-course_t get_nextCourse_address(course_t self);
-void set_nextCourse_address(course_t self, course_t next);
+pcourse_t createCourse(int course_num, char* course_name, int semester_no);
+void destroy_course(pcourse_t course);
+void set_cours_num(pcourse_t course, int course_num);
+void set_course_name(pcourse_t course, char* course_name);
+void set_semester_num(pcourse_t course, int semester_num);
+int get_course_num(pcourse_t course);
+char get_course_name(pcourse_t course);
+int get_semester_num(pcourse_t course);
+void print_course_info(pcourse_t course);
