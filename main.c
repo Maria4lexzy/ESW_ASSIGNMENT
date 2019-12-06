e6d264a68b17ac83a64584ff4757855742a5a951
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2019-12-06 18:45:47 +0100

    teacherList implementation test

--- main.c
+++ main.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include "student.h"
 #include "student_list.h"
-
+#include "teacher.h"
+#include "teacher_list.h"
 
 void main() {
-
-	//creating list
+	//list size for all list
 	int nrOfItems;
+	//creating student list
+
 	printf("Enter total number students: ");
 	scanf_s("%d", &nrOfItems);
 	setStudentListSize(nrOfItems);
-
+	
+	//creating teacher list
+	set_teacher_list_size(nrOfItems);
 	//adding student
-	add_student_to_list(create_student(123456, "Jakub"));
-	add_student_to_list(create_student(723456, "Feri"));
+	add_student_to_list(create_student(123461, "Jakub"));
+	add_student_to_list(create_student(723462, "Feri"));
+	
+	//adding teacher
+	add_teacher_to_list(create_teacher(123461, "Jakubino"));
+	add_teacher_to_list(create_teacher(723462, "Ferino"));
 
-	//checking functionality
+	//checking student functionality
 	printStudentList(); //printing out all items from the linkedList
 	pstudent_t* student_val= get_student_by_index(0);
 	printf("0 POS: %d   %s ", student_val->student_num,student_val->first_name);
@@ -24,6 +32,16 @@ void main() {
 	remove_student_from_list(student_val);
 	destroy_student(student_val);
 	printStudentList();
+
+	//checking teacherfunctionality
+	print_teacher_list(); //printing out all items from the linkedList
+	pteacher_t* teacher_val = get_teacher_by_index(0);
+	printf("0 POS: %d   %s ", teacher_val->teacher_no, teacher_val->f_name);
+	teacher_val = get_teacher_list_size(teacher_val);
+	printf("0 POS: %d   %s ", teacher_val->teacher_no, teacher_val->f_name);
+	remove_teacher_from_list(teacher_val);
+	destroy_teacher(teacher_val);
+	print_teacher_list();
 
 
 
--- teacher.c
+++ teacher.c
@@ -1,13 +1,37 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <stdlib.h>
 #include "teacher.h"
-pteacher_t create_teacher(int teacher_num, char f_name) 
-{ 
-	return *((pteacher_t*)0);
+
+pteacher_t* create_teacher(int teacher_num, char* first_name) {
+	pteacher_t* teacherV = ((pteacher_t*)malloc(sizeof(pteacher_t)));
+	if (teacherV == NULL) {
+		return NULL;
+	}
+	else {
+		teacherV->teacher_no = teacher_num;
+		teacherV->f_name = first_name;
+		return teacherV;
+	}
 }
-void destroy_eacher(pteacher_t teacher){}
-void set_teacher_num(pteacher_t teacher, int teacher_num){}
-void set_teacher_name(pteacher_t teacher, char f_name){}
-int get_teacherNo(pteacher_t teacher) { return 0; }
-char get_teacherFName(pteacher_t teacher) { return ' '; }
+int get_teacherNo(pteacher_t self)
+{
+	return self.teacher_no;
+}
+
+char* get_teacher_name(pteacher_t self)
+{
+	return self.f_name;
+}
+
+void destroy_teacher(pteacher_t* self)
+{
+	free(self);
+	self = NULL;
+}
+
+
 void print_teacher_info(pteacher_t teacher) {
 
 	//printf("T \t %s\t %d\n", teacher->first_name, teacher->teacher_num);
